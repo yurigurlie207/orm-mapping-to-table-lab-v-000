703d073e9f8dff0a09df87de8481d2b9e391d0b5
Adrienne Yao <adrienne.yao@icloud.com>
2019-06-14 16:50:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,11 @@ class Student
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
 
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    sql = <<-SQL
+      SELECT last_insert_rowid()
+      FROM students"
+    SQL
+    @id = DB[:conn].execute(")[0][0]
   end
 
   def self.create(name:, grade:)
